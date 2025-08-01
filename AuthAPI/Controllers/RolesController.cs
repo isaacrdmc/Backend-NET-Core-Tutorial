b3b34576c0f1fcfa9e4a9b4840be19f20a14de5b
Isaac Ramirez <isaacrdmc@gmail.com>
2025-07-31 19:39:42 -0600

    Termine los nuevos servicios del back, ahora falta probarlos

--- AuthAPI/Controllers/RolesController.cs
+++ AuthAPI/Controllers/RolesController.cs
@@ -4,8 +4,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-using API.Dtos;
-using API.Models;
+using AuthAPI.Dtos;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using AuthAPI.Model;
@@ -14,61 +13,126 @@ using AuthAPI.Model;
 
 namespace AuthAPI.Controllers
 {
-    public class RolesController
+    [Authorize(Roles = "Admin")]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RolesController : ControllerBase
     {
-        // Creamos los nuevos endpoints para manejar los roles de los usuarios:
 
-        [Authorize(Roles = "Admin")]
-        [ApiController]
-        [Route("api/[controller]")]
-        public class RolesController:ControllerBase
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<AppUser> _userManager;
+
+        public RolesController(RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager)
         {
-
-            // 
-            private readonly RoleManager<IdentityRole> _roleManager;
-            private readonly UserManager<AppUser> _userManager;
-
-            // 
-            public RolesController(RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager)
-            {
-                _roleManager = roleManager;
-                _userManager = userManager;
-            }
-
-
-            // Creamos un nuevo rol dentro de la base de datos:
-            [HttpPost]
-            public async Task<IActionResult> CreateRole([FromBody] CreateRoleDto createRoleDto)
-            {
-                if (string.IsNullOrEmpty(createRoleDto.RoleName))
-                {
-                    return BadRequest("Role name is required");
-                }
-
-                var roleExist = await _roleManager.RoleExistsAsync(createRoleDto.RoleName);
-
-                if (roleExist)
-                {
-                    return BadRequest("Role already exist");
-                }
-
-                var roleResult = await _roleManager.CreateAsync(new IdentityRole(createRoleDto.RoleName));
-
-                if (roleResult.Succeeded)
-                {
-                    return Ok(new { message = "Role Created successfully" });
-                }
-
-                return BadRequest("Role creation failed.");
-
-            }
-
-
-
-
-            // Endpoint para obtener todos los roles
+            _roleManager = roleManager;
+            _userManager = userManager;
         }
 
 
+        // Creamos un nuevo rol dentro de la base de datos:
+        [HttpPost]
+        public async Task<IActionResult> CreateRole([FromBody] CreateRoleDto createRoleDto)
+        {
+            if (string.IsNullOrEmpty(createRoleDto.RoleName))
+            {
+                return BadRequest("Role name is required");
+            }
+
+            var roleExist = await _roleManager.RoleExistsAsync(createRoleDto.RoleName);
+
+            if (roleExist)
+            {
+                return BadRequest("Role already exist");
+            }
+
+            var roleResult = await _roleManager.CreateAsync(new IdentityRole(createRoleDto.RoleName));
+
+            if (roleResult.Succeeded)
+            {
+                return Ok(new { message = "Role Created successfully" });
+            }
+
+            return BadRequest("Role creation failed.");
+
+        }
+
+
+        // Endpoint para obtener todos los roles junto con la cantidad de usuarios que pertenecen a cada uno de esos roles.
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<RoleResponseDto>>> GetRoles()
+        {
+            var roles = await _roleManager.Roles.Select(r => new RoleResponseDto
+            {
+                Id = r.Id,
+                Name = r.Name,
+                TotalUsers = _userManager.GetUsersInRoleAsync(r.Name!).Result.Count
+            }).ToListAsync();
+
+            return Ok(roles);
+        }
+
+
+        // Endpoint para eliminar un rol de la Base de Datos según el ID:
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRole(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+
+            if (role is null)
+            {
+                return NotFound("Role not found.");
+            }
+
+            var result = await _roleManager.DeleteAsync(role);
+
+            if (result.Succeeded)
+            {
+                return Ok(new { message = "Role deleted successfully." });
+            }
+
+            return BadRequest("Role deletion failed.");
+
+        }
+
+
+        // Endpoint para asignar un rol que ya existe a un usuario por medio de sú ID:
+        [HttpPost("assign")]
+        public async Task<IActionResult> AssignRole([FromBody] RoleAssignDto roleAssignDto)
+        {
+            var user = await _userManager.FindByIdAsync(roleAssignDto.UserId);
+
+            if (user is null)
+            {
+                return NotFound("User not found.");
+            }
+
+            var role = await _roleManager.FindByIdAsync(roleAssignDto.RoleId);
+
+            if (role is null)
+
+            {
+                return NotFound("Role not found.");
+            }
+
+            var result = await _userManager.AddToRoleAsync(user, role.Name!);
+
+            if (result.Succeeded)
+            {
+                return Ok(new { message = "Role assigned successfully" });
+            }
+
+            var error = result.Errors.FirstOrDefault();
+
+            return BadRequest(error!.Description);
+
+        }
+
+
+
+
+
     }
+
+
 }
+
